3f52a445916b97af9b01932cb4beaa5697ffc40d
Muhammad Ramdani <ramdani0794812@gmail.com>
2025-07-16 14:22:18 +0700

    menambahkan jadwal materi minggu ke-3

---
+++ jadwal_minggu3.py
@@ -0,0 +1,9 @@
+jadwal = {
+    'satu' : "membuat list numpy",
+    'dua' : "membuat array",
+    'tiga' : "membuat data matrix"
+}
+
+print(jadwal['satu'])
+print(jadwal['dua'])
+print(jadwal['tiga'])
