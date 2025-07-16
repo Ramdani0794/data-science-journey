de262b75bffddeaa52dfac357251acd3c96c336e
Muhammad Ramdani <ramdani0794812@gmail.com>
2025-07-16 14:08:51 +0700

    menambahkan tugas numpy_minggu3.py sebagai pembelajaran

---
+++ numpy_minggu3.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+array1 = np.array([ (9,5,8), (5,8,6),(9,8,7) ])
+array2 = np.array([ (9,5,8), (5,8,6),(9,8,7) ])
+
+hasil = array1 + array2
+
+print(f"{array1} \n   + \n{array2} \n   =\nhasil \n{hasil}")
