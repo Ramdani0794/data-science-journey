38250b63efa2774b46e798cada6f14a2877a6bd7
ramdani07941 <ramdani0794812@gmail.com>
2025-07-17 13:43:24 +0700

    menambahkan file operasi matematik didalam array termasuk penjumlahan,pengurangan,perkalian,pembagian dan pangkat atau kuadrat

---
+++ operasi_matematika_array.py
@@ -0,0 +1,17 @@
+import numpy as np
+a = np.array ([ (1,2,3),(2,3,4),(5,6,2) ])
+b = np.array ([ (1,2,3),(4,5,6),(7,8,9) ])
+
+tambah = a + b
+kurang = a - b
+bagi = a / b
+kali = a * b
+
+print(a)
+print(b)
+print("\n",tambah)
+print("\n",kurang)
+print("\n",bagi)
+print("\n",kali)
+print("\n",a*2)
+print("\n",a**2)
