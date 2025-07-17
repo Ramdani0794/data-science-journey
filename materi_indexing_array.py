e33f90a28dacb82e704c4f4d04929c4e4f2d9d40
ramdani07941 <ramdani0794812@gmail.com>
2025-07-17 13:15:23 +0700

    menambah materi bentuk indexing dari sebuah array numpy

---
+++ materi_indexing_array.py
@@ -0,0 +1,17 @@
+import numpy as np
+array_1d = np.array([10,20,30,40,50])
+
+print(array_1d[0]) # mengeksekusi nilai array index 0 yaitu 10 (indexing)
+print(array_1d[0:5]) #mengeksekusi nilai array dari index 0 sampai 5 (slicing)
+
+array_2d = np.array([ [10,20,30],[40,50,60],[70,80,90] ]) #array du dimensi ([ [baru diisi angka],[lanjut baris kedua],[lanjut baris ketiga] ])
+print(array_2d)
+
+print(array_2d[0][0]) #mengeksekusi baris nol kolom nol
+print(array_2d[1][2]) #mengeksekusi baris satu kolom kedua
+print(array_2d[0,0:3]) #mengeksekusi baris nol kolom nol sampai tiga
+
+array_sebelum = np.arange(1,10)
+array_sesudah = array_sebelum.reshape((3,3))
+print(array_sebelum)
+print(array_sesudah)
--- numpy_materi.py
+++ numpy_materi.py
@@ -5,7 +5,7 @@ array_list = np.array(list) #array dari list hasil print array tidak akan menggu
 array_nol = np.zeros((2,2)) #array zeros adalah kumpulan array dengan bilangan nol ((2,2)) ini artinya array memiliki 2 baris dan dua kolom 2 di depan artinya baris dan 2 dibelakang artinya kolom
 array_satu = np.ones((4,4)) #array ones adalah array dengan angka 1 sama hal dangan array zeros
 array_dua = np.full((2,2),4) #array full berisi nilai array yang mau kita input ((2,2),4) 2,2 disini adalah kolom dan baris dan 4 adalah angka yang akan dibuat array
-array_range = np.arange(0,9,3)
+array_range = np.arange(0,9,3) #arange adalah array range konsep nya sama yaitu 3s (start,stop,step)
 linspacce = np.linspace(1,10,4) # linspace menciptakan kondisi dimana jumlah array memiliki jarak yang sama dari satu bilangan ke bilangan lainnya
 
 
@@ -17,5 +17,4 @@ print(array_satu)
 print(array_dua)
 print(array_range)
 print(linspacce)
-print()
 print(type(array_list))
